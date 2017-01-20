e7b4916524fd2a15902a2a70cf6dd2701dacdb36
Valtteri <valtsu54@hotmail.com>
2017-01-20 13:15:13 +0200

    testi

---
+++ Miinaharava/src/main/java/miinaharava/logiikka/testiluokka.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miinaharava.logiikka;
+
+/**
+ *
+ * @author vavo
+ */
+public class testiluokka {
+
+    private int c;
+
+    public testiluokka(int a, int b) {
+        this.c = a + b;
+    }
+    
+    public int getC() {
+        return c;
+    }
+}
